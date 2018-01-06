990e89f5baa218ea134d89580aae246114a37244
Nicolas Duchon <nicolas.duchon@gmail.com>
2018-01-06 17:36:37 +0100

    Change ambiguous variable and function names

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -34,7 +34,7 @@ function get_nginx_proxy_cid {
             break
         fi
     done
-    if [[ -z "$(nginx_proxy_container)" ]]; then
+    if [[ -z "$(get_nginx_proxy_container)" ]]; then
         echo "Error: can't get nginx-proxy container id !" >&2
         echo -n "Check that you use the --volumes-from option to mount volumes from "
         echo -n "the nginx-proxy or label the nginx proxy container to use with "
@@ -88,7 +88,7 @@ source /app/functions.sh
 
 if [[ "$*" == "/bin/bash /app/start.sh" ]]; then
     check_docker_socket
-    if [[ -z "$(docker_gen_container)" ]]; then
+    if [[ -z "$(get_docker_gen_container)" ]]; then
         [[ -z "${NGINX_PROXY_CONTAINER:-}" ]] && get_nginx_proxy_cid
     fi
     check_writable_directory '/etc/nginx/certs'
